d1572a7d8c798c879b8b844b095edf52e6f0704b
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-03 13:23:50 -0500

    display incremental game

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Dat Nguyen
+ * Created on: Mar 2026
+ * This program will incease cookies by 1 when A is pressed and set it to 0 when B is pressed.
 */
 
-basic.showString('Hello, World!')
+// Initialize variable cookies.
+let cookies: number = 0
+
+// Button A pressed.
+input.onButtonPressed(Button.A, function() {
+    // Increase cookies by 1 and display.
+    cookies += 1
+    basic.clearScreen()
+    basic.showNumber(cookies)
+})
+
+// Button B pressed.
+input.onButtonPressed(Button.B, function() {
+    // Assign cookies to 0 and display.
+    cookies = 0
+    basic.clearScreen()
+    basic.showNumber(cookies)
+})
