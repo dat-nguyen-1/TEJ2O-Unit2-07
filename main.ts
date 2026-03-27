0a39493ff95f56dc7b1aef2685b9db4b4fbc6711
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-27 12:38:27 -0400

    Refactor comments to use lowercase
    Updated comments for consistency in casing.

--- main.ts
+++ main.ts
@@ -5,28 +5,28 @@
  * This program will increase the number displayed when button A is pressed.
 */
 
-// Initialize variables
+// initialize variables
 let cookies: number = 0
 
 // Initialize display
 basic.showIcon(IconNames.Happy)
 
-// Handle button A press
+// handle button A press
 input.onButtonPressed(Button.A, function() {
-    // Increase cookies by 1
+    // increase cookies by 1
     cookies += 1
 
-    // Display cookies
+    // display cookies
     basic.clearScreen()
     basic.showNumber(cookies)
 })
 
-// Handle button B press
+// handle button B press
 input.onButtonPressed(Button.B, function() {
-    // Reset cookies to 0.
+    // reset cookies to 0.
     cookies = 0
 
-    // Display cookies
+    // display cookies
     basic.clearScreen()
     basic.showNumber(cookies)
 })
